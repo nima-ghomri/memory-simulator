b4ff8edbed055f4068be2cf6a246ed2e0b5fd4dd
Nima Ghomri <nima.ghomri@gmail.com>
2022-02-06 21:14:20 +0330

    Change Byte

--- simulator.js
+++ simulator.js
@@ -4,51 +4,37 @@ class Byte {
     this.index = index;
     this.address =
       "0x" + this.index.toString(16).padStart(8, "0").toUpperCase();
-    this.value = null;
-    this.bits = new Uint8Array(1);
-  }
 
-  getAddress() {
-    return this.address;
+    this.element = $(
+      `<div class="byte">
+        <div class="bits"></div>
+        <div class="value"></div>
+        <div class="address">${this.address}</div>
+      </div>`
+    );
   }
 
   getBits() {
-    return this.bits[0].toString(8).padStart(8, "0");
+    return new Uint8Array(1)[0].toString(8).padStart(8, "0");
   }
 
-  getValue() {
-    return this.value.toString();
-  }
-}
-
-class Memory {
-  constructor(volumn) {
-    this.bytes = [];
-    this.allocate(volumn);
+  setBits(bits){
+    var b  = new Uint8Array([bits])[0].toString(8).padStart(8, "0");
+    this.element.children(".bits").html(b);
   }
 
-  allocate(volumn) {
-    console.log(volumn);
-    for (let index = 0; index < volumn; index++) {
-      let byte = new Byte(index);
-      this.bytes.push(byte);
-      $("#memory").append(
-        $.parseHTML(`
-        <div class="byte">
-          <div class="bits">${byte.getBits()}</div>
-          <div class="value"></div>
-          <div class="address">${byte.getAddress()}</div>
-        </div>
-        `)
-      );
-    }
-
-    // Memory volum cannot be changed
-    if (Object.freeze) Object.freeze(this.bytes);
+  setValue(value) {
+    this.element.children(".value").html(value);
   }
 }
 
-var memory = null;
+class Variable {
+  constructor(type, value) {
+    this.type = type;
+    this.value = value;
+  }
+}
+
 const DATATYPES = {
   bool: { min: "False", max: "True" },
   int: { min: -2147483648, max: 2147483647 },
@@ -57,68 +43,112 @@ const DATATYPES = {
   char: {},
 };
 
-$(document).ready(function () {
-  // Create memory
-  const queryString = window.location.search;
-  const urlParams = new URLSearchParams(queryString);
-  const volumn = urlParams.get("bytes") ?? 128;
-  memory = new Memory(volumn);
-  console.log(memory);
+class Memory {
+  constructor(volumn) {
+    this.bytes = [];
+    this.allocate(volumn);
+    this.selected = this.bytes[0];
+    this.variables = new Map();
+  }
 
+  store() {
+    var type = $("#select-type").val();
+    var name = $("#input-name").val();
+    var value = $("#input-value").val();
+
+    this.variables.set(name, new Variable(type, value));
+    this.bytes[0].setValue(0);
+    this.bytes[0].setBits(1);
+
+    switch (type) {
+      case "string":
+        for (let i = this.selected.index; i < value.length; i++) {
+          console.log(value[i]);
+        }
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  allocate(volumn) {
+    for (let index = 0; index < volumn; index++) {
+      let byte = new Byte(index);
+      this.bytes.push(byte);
+      $("#memory").append(byte.element);
+    }
+
+    // Memory volum cannot be changed
+    if (Object.freeze) Object.freeze(this.bytes);
+  }
+}
+
+$(document).ready(function () {
   // Fill data types drop down menu
   Object.keys(DATATYPES).forEach((data) => {
     $("#select-type").append(`<option value="${data}">${data}</option>`);
   });
 
-  $("#store").on('click',()=>
-  console.log("test")
-  );
+  // Create memory
+  const volumn = new URLSearchParams(window.location.search).get("bytes") ?? 64;
+  memory = new Memory(volumn);
+  console.log(memory);
 
+  document.getElementById("store").addEventListener(
+    "click",
+    function () {
+      memory.store();
+    },
+    false
+  );
   // Value input field change
-  $("#select-type").change((event) => {
-    $("#input-value").remove();
-    let name = event.target.value;
-    let type = DATATYPES[name];
-    let content = "";
-    switch (name) {
-      case "bool":
-        content = `<select id="input-value" name="value" class="form-select" aria-label="True or false selection menu">
-                    <option value="" selected disabled>Value</option>
-                    <option value="${type["min"]}">${type["min"]}</option>
-                    <option value="${type["max"]}">${type["max"]}</option>
-                   </select>`;
-        break;
-      case "int":
-      case "long":
-        content = `<input id="input-value"
-                          name="value" 
-                          type="number"
-                          class="form-control"
-                          placeholder="Value"
-                          aria-label="Value"
-                          min="${type["min"]}"
-                          max="${type["max"]}"/> `;
-        break;
-      case "char":
-        content = `<input id="input-value"
-                          name="value" 
-                          type="text" 
-                          class="form-control" 
-                          placeholder="Value" 
-                          aria-label="Value" 
-                          pattern=".{1}" 
-                          oninvalid="setCustomValidity('Please enter 1 character.')" />`;
-        break;
-      default:
-        content = `<input id="input-value" 
-                          name="value" 
-                          type="text" 
-                          class="form-control" 
-                          placeholder="Value" 
-                          aria-label="Value" 
-                          autocomplete="off"/>`;
-        break;
-    }
-    $(content).insertAfter("#input-name");
-  });
+  $("#select-type").change(changeDataType);
 });
+
+function changeDataType(event) {
+  $("#input-value").remove();
+  let name = event.target.value;
+  let type = DATATYPES[name];
+  let content = "";
+  switch (name) {
+    case "bool":
+      content = `<select id="input-value" name="value" class="form-select" aria-label="True or false selection menu">
+                  <option value="" selected disabled>Value</option>
+                  <option value="${type["min"]}">${type["min"]}</option>
+                  <option value="${type["max"]}">${type["max"]}</option>
+                 </select>`;
+      break;
+    case "int":
+    case "long":
+      content = `<input id="input-value"
+                        name="value" 
+                        type="number"
+                        class="form-control"
+                        placeholder="Value"
+                        aria-label="Value"
+                        min="${type["min"]}"
+                        max="${type["max"]}"/> `;
+      break;
+    case "char":
+      content = `<input id="input-value"
+                        name="value" 
+                        type="text" 
+                        class="form-control" 
+                        placeholder="Value" 
+                        aria-label="Value" 
+                        pattern=".{1}" 
+                        oninvalid="setCustomValidity('Please enter 1 character.')" />`;
+      break;
+    default:
+      content = `<input id="input-value" 
+                        name="value" 
+                        type="text" 
+                        class="form-control" 
+                        placeholder="Value" 
+                        aria-label="Value" 
+                        autocomplete="off"/>`;
+      break;
+  }
+  $(content).insertAfter("#input-name");
+}
